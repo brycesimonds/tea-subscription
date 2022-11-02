7c98f66362c9f4bf8f794760a4a7efeabf11e603
Bryce Simonds <bsimonds14@gmail.com>
2022-11-02 11:03:06 -0600

    Feat: validations complete

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,11 @@
 class Customer < ApplicationRecord
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates_presence_of :address
+
+  has_many :customer_subscriptions
+  has_many :subscriptions, through: :customer_subscriptions
+  has_many :subscription_teas, through: :subscriptions
+  has_many :teas, through: :subscription_teas
 end
