0c5589025a39c7b763476b53a4412787a87290e4
Bryce Simonds <bsimonds14@gmail.com>
2022-11-02 11:09:54 -0600

    Refactor: changed should haves in test

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe Subscription, type: :model do
 
   describe 'relationships' do 
     it { should have_many(:customer_subscriptions) }
-    it { should have_many(:subscriptions).through(:customer_subscriptions) }
-    it { should have_many(:subscription_teas).through(:subscriptions) }
+    it { should have_many(:customers).through(:customer_subscriptions) }
+    it { should have_many(:subscription_teas) }
     it { should have_many(:teas).through(:subscription_teas) }
   end
 end
